4e1c674a6ef59751365bcb3e1042604617cce196
Fernando Éricles <feericles@gmail.com>
2021-11-01 21:24:37 -0300

    first commit

---
+++ lib/list_filter.ex
@@ -0,0 +1,30 @@
+defmodule ListFilter do
+  def call(list) do
+    if Enum.empty?(list) do
+      0
+    else
+      Enum.reduce(list, fn num, acc -> reduce(num, acc) end)
+    end
+  end
+
+  defp reduce(num, acc) do
+    converted_acc = convert(acc)
+    converted_num = convert(num)
+
+    if rem(converted_num, 2) == 0 do
+      converted_acc
+    else
+      converted_acc + 1
+    end
+  end
+
+  defp convert(value) when is_integer(value), do: value
+
+  defp convert(value) when not is_integer(value) do
+    try do
+      String.to_integer(value)
+    rescue
+      _e in ArgumentError -> 0
+    end
+  end
+end
---
+++ test/list_filter_test.exs
@@ -0,0 +1,27 @@
+defmodule ListFilterTest do
+  use ExUnit.Case
+
+  describe "call/1" do
+    test "count odd numbers" do
+      list1 = ["1", "3", "6", "43", "banana", "6", "abc"]
+      list2 = [1, 6, 8, 7, "20", "27", "fernando", "elixir", "49"]
+      list3 = ["1", "3", "43", "rocketseat", "21", "31"]
+
+      list1_expected_response = 3
+      list2_expected_response = 4
+      list3_expected_response = 5
+
+      assert ListFilter.call(list1) == list1_expected_response
+      assert ListFilter.call(list2) == list2_expected_response
+      assert ListFilter.call(list3) == list3_expected_response
+    end
+
+    test "passing an empty list" do
+      list = []
+
+      expected_response = 0
+
+      assert ListFilter.call(list) == expected_response
+    end
+  end
+end
